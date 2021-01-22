59035050953e2359d31ac367bcbb1b8af32c97cd
Hannah Ng <hannahng@hannahs-mbp-2.lan>
2021-01-22 14:29:16 -0800

    optimized for mobile view

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,32 @@
-import React from 'react';
+import React,{useState} from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import avatarphoto from '../assets/avatarphoto.jpg';
+import ListIcon from '@material-ui/icons/List';
 import './Navbar.scss';
 
 export default function Navbar() {
 
-
+    const [menuIsShown, setMenuIsShown] = useState(false)
+    console.log(menuIsShown)
 
     return (
         <div className="navbar">
         <div className="navbar-left">
             <h2>FLUFFY</h2>
-            <ul>
-                <li>Home</li>
-                <li>Resources</li>
-                <li>About</li>
-                <li>Ways to give</li>
-            </ul>
+            <div className="navbar-menu">
+                <div className="toggle-menu" onClick={()=>{setMenuIsShown(!menuIsShown)}}>
+                    <ListIcon fontSize="large"/>
+                </div>
+                <ul className={menuIsShown?"list-visible":"list-hidden"}>
+                    <li>Home</li>
+                    <li>Resources</li>
+                    <li>About</li>
+                    <li>Ways to give</li>
+                    <li className="visible-sm">Adopt</li>
+                    <li className="visible-sm">Compare</li>
+                    <li className="visible-sm">Account</li>
+                </ul>
+                </div>
         </div>
 
         <div className="navbar-right">
